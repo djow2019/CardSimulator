bf9151e468d254a9262e78adee6b5ae8fa770c23
Kyle <kacrews@ucsd.edu>
2016-07-27 17:28:59 -0700

    AddedsomeFiles and start menu

---
+++ app/src/main/java/codersofanarchy/cardsimulator/GameTables/SinglePlayerTable.java
@@ -0,0 +1,7 @@
+package codersofanarchy.cardsimulator.GameTables;
+
+/**
+ * Created by kylecrews on 7/27/16.
+ */
+public class SinglePlayerTable {
+}
---
+++ app/src/main/java/codersofanarchy/cardsimulator/UserAssistanceScenes/OptionsMenu.java
@@ -0,0 +1,7 @@
+package codersofanarchy.cardsimulator.UserAssistanceScenes;
+
+/**
+ * Created by kylecrews on 7/27/16.
+ */
+public class OptionsMenu {
+}

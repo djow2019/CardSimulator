f7284315e50b9c00e65088e1a238942ec5a8275d
DJow <derek.jow@gmail.com>
2016-07-27 17:09:31 -0700

    Title Screen
    basic stuff on the title screen

--- app/src/main/java/codersofanarchy/cardsimulator/StartMenu.java
+++ app/src/main/java/codersofanarchy/cardsimulator/StartMenu.java
@@ -1,5 +1,6 @@
 package codersofanarchy.cardsimulator;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,9 @@ public class StartMenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
     }
 
     @Override
